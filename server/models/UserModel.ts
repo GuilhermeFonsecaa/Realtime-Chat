f979d44671c41b5d7c60e04b1d9f3ae374d9d601
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-10-15 01:54:57 -0300

    fix: user schema image

--- server/models/UserModel.ts
+++ server/models/UserModel.ts
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: false,
     },
-    imnage: {
+    image: {
         type: String,
         required: false
     },
