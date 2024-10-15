a986e28528ed61b4bb4d5495931d8e252d9127ab
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-10-15 01:55:51 -0300

    fix: auth controller login

--- server/controllers/AuthController/login/index.ts
+++ server/controllers/AuthController/login/index.ts
@@ -8,19 +8,19 @@ export const login = async (request: any, response: any) => {
         const { email, password } = request.body;
 
         if (!email || !password) {
-            return response.status(400).send("Email and Password is required.")
+            response.status(400).json("Email e senha são obrigatórios")
         }
 
         const user = await User.findOne({ email });
 
         if (!user) {
-            return response.status(400).send("User with the given email not found.")
+            return response.status(400).json({message:"Usuário com o e-mail fornecido não foi encontrado."})
         }
 
         const auth = await compare(password, user.password)
 
         if (!auth) {
-            return response.status(400).send("Password is incorrect")
+            return response.status(400).json({message:"Senha incorreta"})
         }
 
         const token = createToken({ email: user.email, userId: user.id })
@@ -45,6 +45,6 @@ export const login = async (request: any, response: any) => {
     }
     catch (error) {
         console.log({ error });
-        return response.status(500).send("Internal Server Error");
+        return response.status(500).json({ message: "Erro do Servidor" });
     }
 }
