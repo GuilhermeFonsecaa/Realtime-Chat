441d80ecf84a67a924f47810e413fad1da1a1098
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-10-15 01:02:37 -0300

    feat: add constants

---
+++ client/src/utils/constants.ts
@@ -0,0 +1,8 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const HOST = import.meta.env.VITE_SERVER_URL;
+
+export const AUTH_ROUTES = "api/auth";
+export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`
+
+export const queryClient = new QueryClient()
