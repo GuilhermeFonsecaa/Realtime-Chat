adf21127ca08958e942fb30f6bede590f99698d3
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-10-15 01:03:29 -0300

    feat: add toaster and QueryClientProvider on main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Toaster } from 'sonner'
+import {QueryClientProvider} from "@tanstack/react-query"
+import { queryClient } from './utils/constants.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <Toaster closeButton />
+    </QueryClientProvider>
+  </>
 )
