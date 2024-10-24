11717c74c09f570be961f64d31349dca795a3ea9
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-10-24 00:40:29 -0300

    feat: add colors and getColor

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const colors = [
+  "bg-orange-600 text-orange-100 border-[1px] border-orange-500",
+  "bg-[#712c4a57] text-[#ff006e] border-[1px] border-[#ff006faa]",
+  "bg-rose-700 text-rose-200 border-[1px] border-rose-400",
+  "bg-emerald-500 text-green-200 border-[1px] border-emerald-300"
+];
+
+
+export const getColor = (color: number) => {
+  if (color && color < colors.length) {
+    return colors[color]
+  };
+  return colors[0];
+}
