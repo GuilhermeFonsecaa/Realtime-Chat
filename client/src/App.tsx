2f7b4aadf9db8a33b6ea73afb826d1357bdd5f8c
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-10-24 00:59:05 -0300

    feat: add routes settings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,18 +3,74 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Auth from './pages/auth'
 import Chat from './pages/chat'
 import Profile from './pages/profile'
+import { useAppStore } from './store'
+import { getUserData } from './hooks/getUserData'
+import { useQuery } from '@tanstack/react-query'
+import { ReactNode, useEffect } from 'react'
+
+interface RouteProps {
+  children: ReactNode;
+}
+
+const PrivateRoute = ({ children }: RouteProps) => {
+  const { userInfo } = useAppStore();
+  const isAuthenticated = !!userInfo;
+  return isAuthenticated ? children : <Navigate to="/auth" />
+}
+
+const AuthRoute = ({ children }: RouteProps) => {
+  const { userInfo } = useAppStore();
+  const isAuthenticated = !!userInfo;
+  return isAuthenticated ? <Navigate to="/chat" /> : children
+}
+''
 
 function App() {
+  const { userInfo, setUserInfo } = useAppStore();
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["userInfo"],
+    queryFn: getUserData,
+  });
+
+  useEffect(() => {
+    if (isError) {
+      setUserInfo(undefined)
+    }
+
+    if (data) {
+      setUserInfo(data);
+    }
+  }, [data,isError,setUserInfo]);
+
+
+  if (isLoading) {
+    return <div>Carregando...</div>
+  }
+
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/auth' element={<Auth />} />
-        <Route path='/chat' element={<Chat />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path='/auth' element={
+          <AuthRoute>
+            <Auth />
+          </AuthRoute>} />
+
+        <Route path='/chat' element={
+          <PrivateRoute>
+            <Chat />
+          </PrivateRoute>} />
+
+        <Route path='/profile' element={
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>} />
+
         <Route path='*' element={<Navigate to="/auth" />} />
       </Routes>
     </BrowserRouter>
   )
 }
 
-export default App
+export default App;
