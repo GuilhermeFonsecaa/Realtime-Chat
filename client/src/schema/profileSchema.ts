b30348a855086c8639ed8daf7c513751a9973641
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-11-03 23:52:19 -0300

    fix: profile schema where first name and last name are required at least 1 character

--- client/src/schema/profileSchema.ts
+++ client/src/schema/profileSchema.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const profileSchema = z.object({
     email: z.string().email({ message: "Email é obrigatório" }),
-    firstName: z.string({ message: "Primeiro nome é obrigatório" }),
-    lastName: z.string({ message: "Sobrenome é obrigatório" }),
+    firstName: z.string({ message: "Primeiro nome é obrigatório" }).min(1, {message: "Primeiro nome é obrigatório"}),
+    lastName: z.string({ message: "Sobrenome é obrigatório" }).min(1, {message: "Sobrenome é obrigatório"}),
     color: z.number({message: "Cor é obrigatória"})
 })
 
