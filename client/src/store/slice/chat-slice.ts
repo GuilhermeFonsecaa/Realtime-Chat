17e3944408ea6b4c3c8d0c78e9b110cb7c41e22f
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-11-26 03:04:57 -0300

    feat(client): add addMesage function in createChatSlice

--- client/src/store/slice/chat-slice.ts
+++ client/src/store/slice/chat-slice.ts
@@ -1,22 +1,61 @@
 import { StateCreator } from "zustand";
 import { UserInfo } from "./auth-slice";
 
+export interface Message {
+  _id: string
+  content: string;
+  recipient: { _id: string } | string;
+  sender: { _id: string } | string;
+  timestamp: Date;
+  messageType: string
+}
+
+export interface SelectedChatDataProps {
+  _id: string;
+  email: string;
+  password: string;
+  firstName?: string;
+  lastName?: string;
+  image?: string;
+  color?: number;
+  profileSetup: boolean;
+}
+
 export interface ChatSlice {
-    selectedChatType: string | undefined;
-    selectedChatData: UserInfo | undefined;
-    selectedChatMessages: string[];
-    setSelectedChatType: (selectedChatType: string | undefined) => void;
-    setSelectedChatData: (selectedChatData: UserInfo | undefined) => void;
-    setSelectedChatMessages: (selectedChatMessages: string[]) => void;
-    closeChat: () => void;
+  selectedChatType: string | undefined;
+  selectedChatData: SelectedChatDataProps | undefined;
+  selectedChatMessages: Message[];
+  setSelectedChatType: (selectedChatType: string | undefined) => void;
+  setSelectedChatData: (selectedChatData: SelectedChatDataProps | undefined) => void;
+  setSelectedChatMessages: (selectedChatMessages: Message[]) => void;
+  closeChat: () => void;
+  addMessage: (message: Message) => void;
+}
+
+export const createChatSlice: StateCreator<ChatSlice> = (set, get) => ({
+  selectedChatType: undefined,
+  selectedChatData: undefined,
+  selectedChatMessages: [],
+  setSelectedChatType: (selectedChatType) => set({ selectedChatType }),
+  setSelectedChatData: (selectedChatData) => set({ selectedChatData }),
+  setSelectedChatMessages: (selectedChatMessages) => set({ selectedChatMessages }),
+  closeChat: () => set({ selectedChatData: undefined, selectedChatType: undefined }),
+  addMessage: (message) => {
+    const selectedChatMessages = get().selectedChatMessages;
+    const selectedChatType = get().selectedChatType;
+    set({
+      selectedChatMessages: [
+        ...selectedChatMessages, {
+          ...message,
+          recipient: selectedChatType === "channel" ? message.recipient : typeof message.recipient === "object" && message.recipient !== null
+            ? message.recipient._id
+            : message.recipient,
+
+          sender: selectedChatType === "channel" ? message.sender : typeof message.sender === "object" && message.sender !== null
+            ? message.sender._id
+            : message.sender
+        }
+      ]
+    })
   }
-  
-  export const createChatSlice: StateCreator<ChatSlice> = (set) => ({
-    selectedChatType: undefined,
-    selectedChatData: undefined,
-    selectedChatMessages: [],
-    setSelectedChatType: (selectedChatType) => set({ selectedChatType }),
-    setSelectedChatData: (selectedChatData) => set({ selectedChatData }),
-    setSelectedChatMessages: (selectedChatMessages) => set({ selectedChatMessages }),
-    closeChat: () => set({ selectedChatData: undefined, selectedChatType: undefined }), // Corrigido para selectedChatType
-  });
+});
