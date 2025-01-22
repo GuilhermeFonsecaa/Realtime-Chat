749833fea0df11625fa56758a224be4c45816a5e
Guilherme Fonseca <guilhermao1234@hotmail.com>
2025-01-22 00:32:53 -0300

    feat(client): add upload file hook

---
+++ client/src/hooks/uploadFile.ts
@@ -0,0 +1,12 @@
+import { apiClient } from "@/lib/api-client";
+import { UPLOAD_FILE_ROUTE } from "@/utils/constants";
+
+export const UploadFile = async (file: File) => {
+    if (file) {
+        const formData = new FormData();
+        formData.append("file", file);
+        const response = await apiClient.post(UPLOAD_FILE_ROUTE, formData, { withCredentials: true });
+        return response;
+    }
+}
+
