1d7fc99b075674a92a8303d3ab3d03ebf2446ede
Guilherme Fonseca <guilhermao1234@hotmail.com>
2025-01-28 01:35:59 -0300

    feat(client): add file upload mutation

--- client/src/pages/chat/components/chat-container/components/message-bar/index.tsx
+++ client/src/pages/chat/components/chat-container/components/message-bar/index.tsx
@@ -6,14 +6,17 @@ import {
     PopoverTrigger,
 } from "@/components/ui/popover"
 import { Paperclip, SendHorizonal, SmilePlus } from "lucide-react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import EmojiPicker, { Theme } from "emoji-picker-react"
 import { useAuthStore, useChatStore } from "@/store";
 import { useSocket } from "@/context/SocketContext";
-import { useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { UploadFile } from "@/hooks/uploadFile";
+import { toast } from "sonner";
 
 const MessageBar = () => {
     const [message, setMessage] = useState<string>("");
+    const fileInputRef = useRef<HTMLInputElement>(null);
     const { selectedChatType, selectedChatData } = useChatStore();
     const { userInfo } = useAuthStore();
     const socket = useSocket()
@@ -24,27 +27,65 @@ const MessageBar = () => {
     }
 
     const handleMessage = () => {
-        console.log('Selected chat data:', selectedChatData?._id);
         if (selectedChatType === "contact" && selectedChatData && userInfo) {
             socket?.emit("sendMessage", {
-                sender: userInfo.id ,
+                sender: userInfo.id,
                 content: message,
-                recipient: selectedChatData?._id ,
+                recipient: selectedChatData?._id,
                 messageType: "text",
                 fileUrl: undefined
             })
             setMessage("");
-            queryClient.invalidateQueries({queryKey: ["get-contacts"]})
+            queryClient.invalidateQueries({ queryKey: ["get-contacts"] });
+        };
+    };
+
+    const handleAttachementClick = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    }
+
+    const mutationUploadFile = useMutation({
+        mutationFn: UploadFile,
+        onSuccess: (response) => {
+            if (response?.status === 200 && response.data) {
+                if (selectedChatType === "contact" && selectedChatData && userInfo) {
+                    socket?.emit("sendMessage", {
+                        sender: userInfo.id,
+                        content: undefined,
+                        recipient: selectedChatData?._id,
+                        messageType: "file",
+                        fileUrl: response.data.filePath,
+                    });
+                    queryClient.invalidateQueries({ queryKey: ["get-contacts"] });
+                };
+                toast.success("Arquivo carregado com sucesso", { className: "bg-orange-500 text-white" });
+            }
+        },
+        onError: (error: any) => {
+            if (error.response && error.response.data && error.response.data.message) {
+                toast.error(error.response.data.message);
+            }
+        }
+    })
+
+    const handleAttachementChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        console.log(file)
+        if (file) {
+            mutationUploadFile.mutate(file)
         }
     }
 
     return (
-        <div className="h-[10vh] bg-[rgb(28,29,37)] flex justify-center items-center px-8 mb-6 gap-6">
+        <div className="h-[10vh] bg-[rgb(28,29,37)] flex justify-center items-center px-8 mb-6 gap-6" >
 
             <div className="flex-1 flex bg-[#2a2b33] rounded-md items-center gap-5 ">
                 <Input type="text" className="flex-1 p-10 bg-transparent rounded-md focus:border-none focus:outline-none border-none" placeholder="Mensagem..." value={message} onChange={(e) => setMessage(e.target.value.trimStart())} required />
-                <Button className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all bg-transparent hover:bg-transparent">
+                <Button className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all bg-transparent hover:bg-transparent" onClick={handleAttachementClick}>
                     <Paperclip className="text-orange-400" size={23} />
+                    <Input type="file" className="hidden" ref={fileInputRef} onChange={handleAttachementChange} />
                 </Button>
                 <div className="relative flex">
                     <Popover>
@@ -68,7 +109,7 @@ const MessageBar = () => {
                     <SendHorizonal size={20} />
                 </Button>
             </div>
-        </div>
+        </div >
     );
 }
 
