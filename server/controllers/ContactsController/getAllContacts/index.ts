478fe8e13fc73d1fd4afddb8d2289c7a0214cebb
Guilherme Fonseca <guilhermao1234@hotmail.com>
2025-02-10 01:05:35 -0300

    feat(server): add get all contacts route

---
+++ server/controllers/ContactsController/getAllContacts/index.ts
@@ -0,0 +1,18 @@
+import { Response, Request } from "express"
+import User from "../../../models/UserModel";
+
+export const getAllContacts = async (request: Request, response: Response) => {
+    try {
+        const users = await User.find({ _id: { $ne: request.userId } }, "firstName lastName _id email"); //exclui o proprio id e quais campos quer na query
+      
+        const contacts = users.map((user) => ({
+            label: user.firstName ? `${user.firstName} ${user.lastName}` : user.email
+        }))
+       
+        response.status(200).json({ contacts });
+    }
+
+    catch (error) {
+        response.status(500).send("Internal Server Error")
+    }
+}
--- server/routes/ContactsRoute.ts
+++ server/routes/ContactsRoute.ts
@@ -2,10 +2,11 @@ import {Router} from "express";
 import {verifyToken} from "../middlewares/AuthMiddleware/verifyToken";
 import { searchContacts } from "../controllers/ContactsController/searchContacts";
 import {getContactsForDMList} from "../controllers/ContactsController/getContactsForDMList";
+import {getAllContacts} from "../controllers/ContactsController/getAllContacts"
 
 const contactsRoutes = Router();
 
 contactsRoutes.post("/search",verifyToken, searchContacts);
 contactsRoutes.get("/get-contacts-for-dm", verifyToken, getContactsForDMList);
-
+contactsRoutes.get("/get-all-contacts", verifyToken, getAllContacts)
 export default contactsRoutes;
